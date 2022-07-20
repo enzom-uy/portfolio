f4870ebde6aeb08e11fd7c6ab3229529eb7a0261
enzom-uy <enzom.dev@gmail.com>
2022-07-20 15:25:49 -0300

    Refactor navbar links into separate variable
    Store website links in a reusable variable inside "helpers" folder

--- components/navbar/navbar-links.tsx
+++ components/navbar/navbar-links.tsx
@@ -1,45 +1,68 @@
-import {Flex, Icon, List, ListItem} from '@chakra-ui/react'
+import {HamburgerIcon} from '@chakra-ui/icons'
+import {
+  Flex,
+  Icon,
+  List,
+  ListItem,
+  useBreakpointValue,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  Button
+} from '@chakra-ui/react'
+import {links} from 'helpers/variables'
 import Link from 'next/link'
 import React from 'react'
-import {FaGithub} from 'react-icons/fa'
 
-const links = [
-  {
-    name: 'Works',
-    icon: undefined,
-    href: '/works'
-  },
-  {
-    name: 'Posts',
-    icon: undefined,
-    href: '/posts'
-  },
-  {
-    name: 'Source',
-    icon: FaGithub,
-    href: 'https://github.com/enzom-uy/'
-  }
-]
+const linksInList = links.map((item) => (
+  <ListItem
+    key={item.name}
+    display="flex"
+    alignItems="center"
+    _hover={{
+      textDecoration: 'underline',
+      textUnderlineOffset: '3px'
+    }}
+  >
+    {item.icon !== undefined && <Icon as={item.icon} mr="7.5px" />}
+    <Link href={item.href}>{item.name}</Link>
+  </ListItem>
+))
+
+const linksInMenu = links.map((item) => (
+  <MenuItem
+    key={item.name}
+    display="flex"
+    alignItems="center"
+    _hover={{
+      textDecoration: 'underline',
+      textUnderlineOffset: '3px'
+    }}
+  >
+    {item.icon !== undefined && <Icon as={item.icon} mr="7.5px" />}
+    <Link href={item.href}>{item.name}</Link>
+  </MenuItem>
+))
 
 const NavbarLinks: React.FC = () => {
-  return (
-    <Flex as={List} gap={4}>
-      {links.map((item) => (
-        <ListItem
-          key={item.name}
-          display="flex"
-          alignItems="center"
-          _hover={{
-            textDecoration: 'underline',
-            textUnderlineOffset: '3px'
-          }}
-        >
-          {item.icon !== undefined && <Icon as={item.icon} mr="7.5px" />}
-          <Link href={item.href}>{item.name}</Link>
-        </ListItem>
-      ))}
-    </Flex>
-  )
+  const showMobileMenu = useBreakpointValue({base: true, md: false})
+  if (showMobileMenu === false) {
+    return (
+      <Flex as={List} gap={4}>
+        {linksInList}
+      </Flex>
+    )
+  } else {
+    return (
+      <Menu isLazy>
+        <MenuButton as={Button}>
+          <HamburgerIcon />
+        </MenuButton>
+        <MenuList>{linksInMenu}</MenuList>
+      </Menu>
+    )
+  }
 }
 
 export default NavbarLinks
---
+++ helpers/variables.ts
@@ -0,0 +1,19 @@
+import {FaGithub} from 'react-icons/fa'
+
+export const links = [
+  {
+    name: 'Works',
+    icon: undefined,
+    href: '/works'
+  },
+  {
+    name: 'Posts',
+    icon: undefined,
+    href: '/posts'
+  },
+  {
+    name: 'Source',
+    icon: FaGithub,
+    href: 'https://github.com/enzom-uy/'
+  }
+]
