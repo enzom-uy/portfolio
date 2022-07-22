8a9889377e00d0e877fc3ba4c8e39b495617dbab
enzom-uy <enzom.dev@gmail.com>
2022-07-22 15:24:02 -0300

    Delete testing code

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,15 @@
-import CustomNumber from 'components/animated-number/custom-number'
-import Card from 'components/info-contact-section/card'
 import Main from 'components/layouts/main'
 import useTwitter from 'hooks/useTwitter'
 import type { NextPage } from 'next'
 import Head from 'next/head'
 
 const Home: NextPage = () => {
-  const { twitter } = useTwitter()
+  // const { twitter } = useTwitter()
   return (
     <Main>
       <Head>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Card />
-      <CustomNumber value={twitter?.followings} />
     </Main>
   )
 }
