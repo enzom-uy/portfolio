5e01b26798a4d33caa870b3da030fc2e6df7cd3a
enzom-uy <enzom.dev@gmail.com>
2022-07-22 01:13:32 -0300

    Test Animated Number

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,20 @@
+import CustomNumber from 'components/animated-number/custom-number'
+import Card from 'components/info-contact-section/card'
+import Main from 'components/layouts/main'
+import useTwitter from 'hooks/useTwitter'
 import type { NextPage } from 'next'
 import Head from 'next/head'
 
 const Home: NextPage = () => {
+  const { twitter } = useTwitter()
   return (
-    <>
+    <Main>
       <Head>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-    </>
+      <Card />
+      <CustomNumber value={twitter?.followings} />
+    </Main>
   )
 }
 
