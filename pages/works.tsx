c7a30c99eb117a1266be7dd83b09b35808cfd2bb
enzom-uy <enzom.dev@gmail.com>
2022-07-22 15:25:30 -0300

    Create /works route

---
+++ pages/works.tsx
@@ -0,0 +1,9 @@
+import { Flex } from '@chakra-ui/react'
+import { NextPage } from 'next'
+import React from 'react'
+
+const Works: NextPage = () => {
+  return <Flex>This is a test from the /works page</Flex>
+}
+
+export default Works
