91044d795c231ace2ce0e00f74afe71c1b5cc709
enzom-uy <enzom.dev@gmail.com>
2022-07-26 20:02:32 -0300

    Add BriefInfoCard to index

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,15 +2,20 @@ import React from 'react'
 import Main from 'components/layouts/main'
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import { Box } from '@chakra-ui/react'
+import BriefBioCard from 'components/bio-section/brief-bio-card'
 
 const Home: NextPage = () => {
   return (
-    <Main>
+    <>
       <Head>
         <title>Enzo Muñoz - Homepage</title>
         <meta name="description" content="Enzo's Homepage" />
       </Head>
-    </Main>
+      <Box as="section">
+        <BriefBioCard />
+      </Box>
+    </>
   )
 }
 
