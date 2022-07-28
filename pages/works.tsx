e9b36fd4306354b0fedd5dc2f3309874a8c7bdef
enzom-uy <enzom.dev@gmail.com>
2022-07-28 17:49:06 -0300

    Start doing /works page

--- pages/works.tsx
+++ pages/works.tsx
@@ -1,9 +1,19 @@
-import { Flex } from '@chakra-ui/react'
+import { Box, Flex } from '@chakra-ui/react'
 import { NextPage } from 'next'
+import Head from 'next/head'
 import React from 'react'
 
 const Works: NextPage = () => {
-  return <Flex>This is a test from the /works page</Flex>
+  return (
+    <>
+      <Head>
+        <title>Enzo Muñoz - Works</title>
+        <meta name="description" content="Enzo's Works" />
+      </Head>
+
+      <Box as="section">Works page</Box>
+    </>
+  )
 }
 
 export default Works
