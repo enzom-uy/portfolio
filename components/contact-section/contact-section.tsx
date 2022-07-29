ed3972e76c4b132e8f20b5c390b9c405bb088718
enzom-uy <enzom.dev@gmail.com>
2022-07-29 00:13:48 -0300

    No hover effects if on mobile

--- components/contact-section/contact-section.tsx
+++ components/contact-section/contact-section.tsx
@@ -1,6 +1,7 @@
 import { Flex, Icon, Link, Text } from '@chakra-ui/react'
 import SimpleContainer from 'components/containers/simple-container'
 import SectionTitle from 'components/text/section-title'
+import useViewport from 'hooks/useViewport'
 import React from 'react'
 import { IconType } from 'react-icons'
 import { FaGithub, FaLinkedin, FaTwitter } from 'react-icons/fa'
@@ -28,6 +29,7 @@ const contactInfo: Array<{
 ]
 
 const ContactSection: React.FC = () => {
+  const { isMobile } = useViewport()
   return (
     <SimpleContainer flexDir="column">
       <SectionTitle textDecorationColor="green.400">Contact me</SectionTitle>
@@ -44,11 +46,15 @@ const ContactSection: React.FC = () => {
             href={item.href}
             target="_blank"
             rel="noreferrer"
-            _hover={{
-              backgroundColor: '#ffffff',
-              color: '#000000',
-              textDecoration: 'none'
-            }}
+            _hover={
+              isMobile
+                ? undefined
+                : {
+                    backgroundColor: '#ffffff',
+                    color: '#000000',
+                    textDecoration: 'none'
+                  }
+            }
             width="fit-content"
             px={4}
             py={2}
